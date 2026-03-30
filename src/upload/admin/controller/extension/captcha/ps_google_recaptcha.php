a39cf34a4786ad2c80d9ae8ee55063c3c1b7f751
zsoltoroszlany82 <zsolt.oroszlany@playfulsparkle.com>
2026-03-30 17:58:43 +0200

    Update translation, add support website URL

--- src/upload/admin/controller/extension/captcha/ps_google_recaptcha.php
+++ src/upload/admin/controller/extension/captcha/ps_google_recaptcha.php
@@ -7,9 +7,14 @@ class ControllerExtensionCaptchaPsGoogleReCaptcha extends Controller
     const EXTENSION_EMAIL = 'support@playfulsparkle.com';
 
     /**
-     * @var string The documentation URL for the extension.
+     * @var string The URL to the support website.
      */
-    const EXTENSION_DOC = 'https://github.com/playfulsparkle/oc3_google_recaptcha.git';
+    const SUPPORT_URL = 'https://support.playfulsparkle.com';
+
+    /**
+     * @var string The GitHub repository URL of the extension.
+     */
+    const GITHUB_REPO_URL = 'https://github.com/playfulsparkle/oc3_google_recaptcha';
 
     private $error = array();
 
@@ -268,7 +273,7 @@ class ControllerExtensionCaptchaPsGoogleReCaptcha extends Controller
                 fclose($error_log_handle);
             }
         }
-        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::EXTENSION_EMAIL, self::EXTENSION_EMAIL, self::EXTENSION_DOC);
+        $data['text_contact'] = sprintf($this->language->get('text_contact'), self::SUPPORT_URL, self::GITHUB_REPO_URL, self::EXTENSION_EMAIL);
 
         $data['header'] = $this->load->controller('common/header');
         $data['column_left'] = $this->load->controller('common/column_left');
